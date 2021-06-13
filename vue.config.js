6eb3756dec131c86142596c449f727e57b84af28
yhbinbin <yorbin@163.com>
2021-06-13 16:37:03 +0800

    更改

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	// publicPath: process.env.NODE_ENV === 'production'
-	// 	? '/myWebsite/'
-	// 	: '/',
-	publicPath: '/'
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/myWebsite/'
+		: '/',
+	// publicPath: '/'
 }
